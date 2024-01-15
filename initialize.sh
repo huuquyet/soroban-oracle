c85962a47ba847ebe1a5f565617ba0e5cbc93cef
Quyet Nguyen <huuquyetng@gmail.com>
2024-01-15 10:20:39 +0000

    feat: move bindings to initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -8,8 +8,8 @@ SOROBAN_RPC_HOST="$2"
 
 PATH=./target/bin:$PATH
 
-if [[ -f "./.soroban-example-dapp/crowdfund_id" ]]; then
-  echo "Found existing './.soroban-example-dapp' directory; already initialized."
+if [[ -f "./.soroban/contracts/oracle" ]]; then
+  echo "Found existing '.soroban/contracts' directory; already initialized."
   exit 0
 fi
 
@@ -32,7 +32,7 @@ if [[ "$SOROBAN_RPC_HOST" == "" ]]; then
     SOROBAN_RPC_URL="$SOROBAN_RPC_HOST/soroban/rpc"
   fi
 else 
-  SOROBAN_RPC_URL="$SOROBAN_RPC_HOST"  
+  SOROBAN_RPC_URL="$SOROBAN_RPC_HOST"
 fi
 
 case "$1" in
@@ -59,58 +59,62 @@ soroban config network add \
   --rpc-url "$SOROBAN_RPC_URL" \
   --network-passphrase "$SOROBAN_NETWORK_PASSPHRASE" "$NETWORK"
 
-echo "Add $NETWORK to .soroban-example-dapp for use with npm scripts"
-mkdir -p .soroban-example-dapp
-echo $NETWORK > ./.soroban-example-dapp/network
-echo $SOROBAN_RPC_URL > ./.soroban-example-dapp/rpc-url
-echo "$SOROBAN_NETWORK_PASSPHRASE" > ./.soroban-example-dapp/passphrase
+echo "Add $NETWORK to shared config"
 echo "{ \"network\": \"$NETWORK\", \"rpcUrl\": \"$SOROBAN_RPC_URL\", \"networkPassphrase\": \"$SOROBAN_NETWORK_PASSPHRASE\" }" > ./src/shared/config.json
 
 if !(soroban config identity ls | grep token-admin 2>&1 >/dev/null); then
-  echo "Create the token-admin identity"
+  echo "Create the token-admin & donation identity"
   soroban config identity generate token-admin --network $NETWORK
+  soroban config identity generate donation --network $NETWORK
 fi
 ADMIN_ADDRESS="$(soroban config identity address token-admin)"
 
 # This will fail if the account already exists, but it'll still be fine.
-echo "Fund token-admin account from friendbot"
+echo "Fund token-admin & donation account from friendbot"
 soroban config identity fund token-admin --network $NETWORK
+soroban config identity fund donation --network $NETWORK
 # curl --silent -X POST "$FRIENDBOT_URL?addr=$ADMIN_ADDRESS" >/dev/null
 
 ARGS="--network $NETWORK --source token-admin"
 
+WASM_PATH="./target/wasm32-unknown-unknown/release/"
+TOKEN_PATH=$WASM_PATH"soroban_token_contract"
+DONATION_PATH=$WASM_PATH"soroban_donation_contract"
+ORACLE_PATH=$WASM_PATH"soroban_oracle_contract"
+
 # Compiles the smart contracts and stores WASM files in ./target/wasm32-unknown-unknown/release
 echo "Build contracts"
 soroban contract build
+echo "Optimizing contracts"
+soroban contract optimize --wasm $TOKEN_PATH".wasm"
+soroban contract optimize --wasm $DONATION_PATH".wasm"
+soroban contract optimize --wasm $ORACLE_PATH".wasm"
 
-# Deploys the contracts and stores the contract IDs in .soroban-example-dapp
+# Deploys the contracts and stores the contract IDs in .soroban
 
 # The BTC Token contract is a Soroban token that represents BTC/USD
 echo "Deploy the BTC TOKEN contract"
 BTC_TOKEN_ID="$(
   soroban contract deploy $ARGS \
-    --wasm target/wasm32-unknown-unknown/release/soroban_token_contract.wasm
+    --wasm $TOKEN_PATH".optimized.wasm"
 )"
 echo "Contract deployed succesfully with ID: $BTC_TOKEN_ID"
-echo -n "$BTC_TOKEN_ID" > .soroban-example-dapp/btc_token_id
 
 # The donation contract is a Soroban contract that allows users to donate to a specific address
 echo "Deploy the DONATION contract"
 DONATION_ID="$(
   soroban contract deploy $ARGS \
-    --wasm target/wasm32-unknown-unknown/release/soroban_donation_contract.wasm
+    --wasm $DONATION_PATH".optimized.wasm"
 )"
 echo "Contract deployed succesfully with ID: $DONATION_ID"
-echo -n "$DONATION_ID" > .soroban-example-dapp/donation_id
 
 # The oracle contract is responsible for calculating the price of BTC/USD
 echo "Deploy the ORACLE contract"
 ORACLE_ID="$(
   soroban contract deploy $ARGS \
-    --wasm target/wasm32-unknown-unknown/release/soroban_oracle_contract.wasm
+    --wasm $ORACLE_PATH".optimized.wasm"
 )"
 echo "Contract deployed succesfully with ID: $ORACLE_ID"
-echo "$ORACLE_ID" > .soroban-example-dapp/oracle_id
 
 # Initialize the contracts
 echo "Initialize the BTC TOKEN contract"
@@ -122,8 +126,7 @@ soroban contract invoke \
   --symbol BTC \
   --decimal 8 \
   --name Bitcoin \
-  --admin "$ADMIN_ADDRESS"
-echo "Done"
+  --admin token-admin
 
 # Recipient is the only account that can withdraw BTC from the donation contract
 # Cannot make donations
@@ -133,9 +136,8 @@ soroban contract invoke \
   --id "$DONATION_ID" \
   -- \
   initialize \
-  --recipient GCSXUXZSA2VEXN5VGOWE5ODAJLC575JCMWRJ4FFRDWSTRCJYQK4ML6V3 \
-  --token "$BTC_TOKEN_ID" 
-echo "Done"
+  --recipient donation \
+  --token "$BTC_TOKEN_ID"
 
 # Relayer is the account that will be used to relay transactions to the oracle contract
 echo "Initialize the ORACLE contract"
@@ -144,8 +146,14 @@ soroban contract invoke \
   --id "$ORACLE_ID" \
   -- \
   initialize \
-  --caller GCSXUXZSA2VEXN5VGOWE5ODAJLC575JCMWRJ4FFRDWSTRCJYQK4ML6V3 \
+  --caller donation \
   --pair_name BTC_USDT \
   --epoch_interval 600 \
-  --relayer GCSXUXZSA2VEXN5VGOWE5ODAJLC575JCMWRJ4FFRDWSTRCJYQK4ML6V3
+  --relayer donation
+
+echo "Generate bindings contracts"
+soroban contract bindings typescript --network $NETWORK --id $BTC_TOKEN_ID --wasm $TOKEN_PATH".optimized.wasm" --output-dir ./.soroban/contracts/token --overwrite
+soroban contract bindings typescript --network $NETWORK --id $DONATION_ID --wasm $DONATION_PATH".optimized.wasm" --output-dir ./.soroban/contracts/donation --overwrite
+soroban contract bindings typescript --network $NETWORK --id $ORACLE_ID --wasm $ORACLE_PATH".optimized.wasm" --output-dir ./.soroban/contracts/oracle --overwrite
+
 echo "Done"
